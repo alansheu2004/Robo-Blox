7104cd2a57d708a072ea697ca2b7b8f574346649
me1234q <me1234q.me1234q@gmail.com>
2019-02-02 17:01:36 -0800

    Initial

---
+++ newBlocks.js
@@ -0,0 +1,57 @@
+Blockly.Blocks['robot_drive_timed'] = {
+  init: function() {
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(300);
+
+	this.appendValueInput('TIME')
+		.appendField('move robot')
+		.appendField(new Blockly.FieldDropdown([['forward', 'FORWARD'], ['backward', 'BACKWARD']]), 'MODE')
+		.appendField('for');
+	this.appendDummyInput()
+		.appendField('seconds');
+  }
+};
+
+Blockly.Blocks['robot_drive_distance'] = {
+  init: function() {
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(300);
+
+	this.appendValueInput('DISTANCE')
+		.appendField('move robot')
+		.appendField(new Blockly.FieldDropdown([['forward', 'FORWARD'], ['backward', 'BACKWARD']]), 'MODE')
+	this.appendDummyInput()
+		.appendField('feet');
+  }
+};
+
+Blockly.Blocks['robot_turn_timed'] = {
+  init: function() {
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(300);
+
+	this.appendValueInput('TIME')
+		.appendField('turn robot')
+		.appendField(new Blockly.FieldDropdown([['left', 'LEFT'], ['right', 'RIGHT']]), 'MODE')
+		.appendField('for');
+	this.appendDummyInput()
+		.appendField('seconds');
+  }
+};
+
+Blockly.Blocks['robot_turn_degrees'] = {
+  init: function() {
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(300);
+
+	this.appendValueInput('DEGREES')
+		.appendField('turn robot')
+		.appendField(new Blockly.FieldDropdown([['left', 'LEFT'], ['right', 'RIGHT']]), 'MODE')
+	this.appendDummyInput()
+		.appendField('degrees');
+  }
+};
